f921699aab4f0d00de202f85f33e82460d55aa2d
21963 <2196374294@qq.com>
2026-03-02 11:28:33 +0800

    提交app.py

---
+++ FlaskDatabase/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:root@127.0.0.1:3306/flask'
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World! !!'
+
+
+if __name__ == '__main__':
+    app.run()
